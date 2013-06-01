ea66e60a547f5287890948fba6cb0fc78ca3df67
Devon Jones <devon@knewton.com>
2013-06-01 07:41:17 -0600

    bunch of changes to support zsh

--- bash_aliases.d/aws-env-aliases.sh
+++ bash_aliases.d/aws-env-aliases.sh
@@ -1,7 +1,4 @@
 
-alias saws='aws-env -e staging > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv ; echo "${AWS_ACCOUNT}"'
-alias paws='aws-env -e staging > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv ; echo "${AWS_ACCOUNT}"'
-alias uaws='aws-env -e staging > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv ; echo "${AWS_ACCOUNT}"'
-alias sawsf='aws-env -e staging --file > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv ; echo "${AWS_ACCOUNT}"'
-alias pawsf='aws-env -e staging --file > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv ; echo "${AWS_ACCOUNT}"'
-alias uawsf='aws-env -e staging --file > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv ; echo "${AWS_ACCOUNT}"'
+alias saws='aws-env -e staging > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
+alias paws='aws-env -e production > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
+alias uaws='aws-env -e utility > ~/.k.aws/$$.currenv ; source ~/.k.aws/$$.currenv ; rm ~/.k.aws/$$.currenv'
