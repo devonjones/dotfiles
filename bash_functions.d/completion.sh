f877e419b00380c4addf40549ffd8dd4c8bcfb97
“Devon <devon.jones@sendgrid.com>
2014-03-27 10:40:37 -0600

    Initial changes for OSX

--- bash_functions.d/completion.sh
+++ bash_functions.d/completion.sh
@@ -1,3 +1,6 @@
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+        . `brew --prefix`/etc/bash_completion
+fi
 # auto-completion is not case sensitive anymore
 set completion-ignore-case On
 
