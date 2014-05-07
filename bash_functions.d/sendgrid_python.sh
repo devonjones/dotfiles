f5d0ec4f41e8d409f14544942f2ab4c4d1fe5323
Devon Jones <devonjones@jumpbox-005.sjc1.sendgrid.net>
2014-05-07 21:34:10 +0000

    adding /usr/bin/virtualenvwrapper.sh

--- bash_functions.d/sendgrid_python.sh
+++ bash_functions.d/sendgrid_python.sh
@@ -2,6 +2,10 @@ if [ -f /usr/local/bin/virtualenvwrapper.sh ]
 then
         export WORKON_HOME=$HOME/.virtualenvs
         source /usr/local/bin/virtualenvwrapper.sh
+elif [ -f /usr/bin/virtualenvwrapper.sh ]
+then
+        export WORKON_HOME=$HOME/.virtualenvs
+        source /usr/bin/virtualenvwrapper.sh
 fi
 if [ -f /usr/share/python/ns/virtualenvwrapper ]
 then
