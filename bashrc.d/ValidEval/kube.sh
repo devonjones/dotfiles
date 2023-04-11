bd80fc825ea9f4bb232c2ca2889df753ae500ada
Devon Jones <devon@valideval.com>
2023-04-11 11:55:05 -0600

    updates from real env

--- bashrc.d/ValidEval/kube.sh
+++ bashrc.d/ValidEval/kube.sh
@@ -1,8 +1,1 @@
-#export KUBECONFIG=~/.kube/ve-sandbox
-
-source <(kubectl completion bash)
-source <(flux completion bash)
-
-export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
-
 pyenv activate valideval
--- zshrc.d/020.kube.sh
+++ zshrc.d/020.kube.sh
@@ -1,3 +1,6 @@
 
+# Kubectl
+command -v kubectl >/dev/null && source <(kubectl completion zsh)
+
 # Flux
 command -v flux >/dev/null && . <(flux completion zsh)
